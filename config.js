c0d9da8df3d717861867b40c54d493b1397ea0b4
Domen Žagar <zagar.domen@gmail.com>
2015-03-13 19:14:11 +0100

    Corrected db change
    This reverts commit f4e40e9dd9ac9f529b3a59e8fe47035774e510a5.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	"port" : process.env.PORT || 8080,
-	"database" : 'localhost:27017/RESTtest',
+	"database" : 'mongodb://a:a@ds043447.mongolab.com:43447/heroku_app34826715',
 	"secret" : "ilikedthedaythatcould"
 }
